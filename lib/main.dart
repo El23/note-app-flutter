4b6ceedc626edce80c8f349e753a2fff21acc8e5
Elen <elenneg23@gmail.com>
2021-01-23 22:56:05 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app_note/views/note_list.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Note Demo",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+
+      ),
+      home: NoteList(),
+    );
+  }
+}
---
+++ lib/views/note_list.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_note/models/note_for_listing.dart';
+
+class NoteList extends StatelessWidget{
+final notes = [
+  new NoteForListing(
+    noteID: '1',
+    createDateTime: DateTime.now(),
+    latestEditDateTime: DateTime.now(),
+    noteTitle: ' Note №1 '
+  ),
+  new NoteForListing(
+    noteID: '2',
+    createDateTime: DateTime.now(),
+    latestEditDateTime: DateTime.now(),
+    noteTitle: 'Note №2'
+  ),
+  new NoteForListing(
+    noteID: '3',
+    createDateTime: DateTime.now(),
+    latestEditDateTime: DateTime.now(),
+    noteTitle: 'Note №3'
+  )
+
+];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('List of Notes'),),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+
+        },
+        child: Icon(Icons.add),
+      ),body: ListView.separated(
+      separatorBuilder: (_,__) => Divider(height: 1, color: Colors.black38,),
+      itemBuilder: (_, index){
+        return ListTile(
+          title: Text(
+            'hello',
+            style:  TextStyle(color: Theme.of(context).primaryColor),
+          ),
+          subtitle: Text('last adited at..'),
+        );
+      },itemCount: 25,
+    ),
+    );
+  }
+}
